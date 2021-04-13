f1510e579da0fbec51829abc397913e5deabfcc4
Fredrik <fredrvaa@stud.ntnu.no>
2021-04-13 15:16:33 +0200

    Complete setup with redux and thunks

---
+++ src/components/courses/CoursesPage.js
@@ -0,0 +1,65 @@
+import React from "react";
+import { connect } from "react-redux";
+import * as courseActions from "../../redux/actions/courseActions";
+import * as authorActions from "../../redux/actions/authorActions";
+import PropTypes from "prop-types";
+import { bindActionCreators } from "redux";
+import CourseList from "./CourseList";
+
+class CoursesPage extends React.Component {
+  componentDidMount() {
+    if (this.props.courses.length === 0) {
+      this.props.actions.loadCourses().catch((error) => {
+        alert("Loading courses failed" + error);
+      });
+    }
+
+    if (this.props.authors.length === 0) {
+      this.props.actions.loadAuthors().catch((error) => {
+        alert("Loading authors failed" + error);
+      });
+    }
+  }
+
+  render() {
+    return (
+      <>
+        <h2>Course</h2>
+        <CourseList courses={this.props.courses} />
+      </>
+    );
+  }
+}
+
+CoursesPage.propTypes = {
+  authors: PropTypes.array.isRequired,
+  courses: PropTypes.array.isRequired,
+  actions: PropTypes.object.isRequired,
+};
+
+function mapStateToProps(state) {
+  return {
+    courses:
+      state.authors.length === 0
+        ? []
+        : state.courses.map((course) => {
+            return {
+              ...course,
+              authorName: state.authors.find((a) => a.id === course.authorId)
+                .name,
+            };
+          }),
+    authors: state.authors,
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: {
+      loadCourses: bindActionCreators(courseActions.loadCourses, dispatch),
+      loadAuthors: bindActionCreators(authorActions.loadAuthors, dispatch),
+    },
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
---
+++ src/redux/actions/courseActions.js
@@ -0,0 +1,23 @@
+import * as actionTypes from "./actionTypes";
+import * as courseApi from "../../api/courseApi";
+
+export function createCourse(course) {
+  return { type: actionTypes.CREATE_COURSE, course };
+}
+
+export function loadCoursesSuccess(courses) {
+  return { type: actionTypes.LOAD_COURSES_SUCCESS, courses };
+}
+
+export function loadCourses() {
+  return function (dispatch) {
+    return courseApi
+      .getCourses()
+      .then((courses) => {
+        dispatch(loadCoursesSuccess(courses));
+      })
+      .catch((error) => {
+        throw error;
+      });
+  };
+}
